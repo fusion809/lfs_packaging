51b3638271d4edc4607ab03d06cf2c2a6be5b0c2
Brenton Horne <brentonhorne77@gmail.com>
2026-02-25 11:14:11 +1000

    fastfetch and hyfetch

---
+++ fastfetch/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+NAME=fastfetch
+VERSION=$(wget -cqO- https://github.com/fastfetch-cli/fastfetch/releases | grep "releases/tag/" | head -n 1 | cut -d '"' -f 6 | cut -d '/' -f 6)
+
+if ! [[ -d fastfetch ]]; then
+	git clone https://github.com/fastfetch-cli/fastfetch
+fi
+
+cd fastfetch
+git checkout $VERSION
+mkdir -p build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=/usr
+cmake --build . --target fastfetch
+sudo cmake --install .
---
+++ hyfetch/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+NAME=hyfetch
+VERSION=$(wget -cqO- https://github.com/hykilpikonna/hyfetch/releases | grep "releases/tag/" | head -n 1 | cut -d '"' -f 6 | cut -d '/' -f 6)
+if ! [[ -d hyfetch ]]; then
+	git clone https://github.com/hykilpikonna/hyfetch
+fi
+
+cd hyfetch
+git checkout $VERSION
+export PATH=$PATH:/opt/rustc/bin
+cargo fetch --locked --target "$(rustc --print host-tuple)"
+cargo build --frozen --release --all-features
+cargo test --frozen --all-features
+
+sudo install -Dm 755 "target/release/${NAME}" "/usr/bin/${NAME}"
+sudo install -Dm 644 "docs/${NAME}.1" "/usr/share/man/man1/${NAME}.1"
+sudo install -Dm 644 "${NAME}/scripts/autocomplete.bash" "/usr/share/bash-completion/completions/${NAME}"
+sudo install -Dm 644 "${NAME}/scripts/autocomplete.zsh" "/usr/share/zsh/site-functions/_${NAME}"
+sudo install -Dm 644 README.md "/usr/share/doc/${NAME}-$VERSION/README.md"
+
