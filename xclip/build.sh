76842d4dd2fd8df6e8a990255e3ba3e4b20dca51
Brenton Horne <brentonhorne77@gmail.com>
2026-02-28 00:32:50 +1000

    Initial commit

---
+++ xclip/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+NAME=xclip
+VERSION=$(wget -cqO- https://github.com/astrand/xclip/commit/master | grep "clip@" | head -n 1 | sed 's/.* · astrand\/xclip@//g' | sed 's/ · GitHub<\/title>//g')
+
+if ! [[ -d xclip ]]; then
+	git clone https://github.com/astrand/xclip
+fi
+
+cd xclip
+git pull origin master
+autoreconf
+./configure --prefix=/usr
+make -j$(nproc)
+sudo make install
+sudo make install.man
