443930a24226b4bdf9e2915611773fb1de525038
Brenton Horne <brentonhorne77@gmail.com>
2026-02-28 00:00:54 +1000

    Adding leptonica

---
+++ leptonica/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+NAME="leptonica"
+VERSION=$(wget -cqO- https://github.com/DanBloomberg/leptonica/releases | grep "/tag/" | head -n 1 | cut -d '"' -f 6 | cut -d '/' -f 6)
+filename="$NAME-$VERSION.tar.gz"
+if ! [[ -f $filename ]]; then
+	wget -c https://github.com/DanBloomberg/leptonica/archive/ref/tags/${VERSION}.tar.gz -O $filename
+fi
+rm -rf ${filename/.tar.gz/}
+tar xf $filename
+cd ${filename/.tar.gz/}
+./autogen.sh --prefix=/usr
+./configure --prefix=/usr
+make -j$(nproc)
+sudo make install
