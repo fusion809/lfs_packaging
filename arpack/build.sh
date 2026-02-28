230e6d883d3e9ec8e7e6d258ff5c19b5ff987d66
Brenton Horne <brentonhorne77@gmail.com>
2026-02-28 21:38:31 +1000

    Fixing arpack and suitesparse names

--- arpack/build.sh
+++ arpack/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 depends=(openmpi)
-NAME="arpack-ng"
+NAME="arpack"
+PRGNAME="arpack-ng"
 VERSION=$(wget -cqO- https://github.com/opencollab/arpack-ng/tags | grep ".tar.gz" | head -n 1 | cut -d '"' -f 4 | cut -d '/' -f 7 | sed 's/.tar.gz//g')
-filename="$NAME-$VERSION.tar.gz"
+filename="$PRGNAME-$VERSION.tar.gz"
 if ! [[ -f $filename ]]; then
 	wget -c https://github.com/opencollab/arpack-ng/archive/$VERSION.tar.gz -O $filename
 fi
--- suitesparse/build.sh
+++ suitesparse/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 depends=(blas lapack)
-NAME=SuiteSparse
+PRGNAME=SuiteSparse
+NAME=suitesparse
 VERSION=$(wget -cqO- https://github.com/DrTimothyAldenDavis/SuiteSparse/releases | grep "releases/tag/v" | head -n 1 | cut -d '"' -f 6 | cut -d '/' -f 6 | sed 's/^v//g')
-filename="$NAME-$VERSION.tar.gz"
+filename="$PRGNAME-$VERSION.tar.gz"
 direname="${filename/.tar.gz/}"
 wget -c https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/v$VERSION.tar.gz -O $filename
 rm -rf $direname
