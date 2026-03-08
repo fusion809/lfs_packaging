6d85201eb59b050335409467a130b2096beebdfc
Brenton Horne <brentonhorne77@gmail.com>
2026-03-08 21:45:03 +1000

    Fixing versioning of antigravity

--- antigravity/build.sh
+++ antigravity/build.sh
@@ -2,15 +2,15 @@
 set -e
 name=antigravity
 baseurl="https://us-central1-apt.pkg.dev/projects/antigravity-auto-updater-dev/dists/antigravity-debian/main/binary-amd64/Packages"
-downurl="https://us-central1-apt.pkg.dev/projects/antigravity-auto-updater-dev/pool/antigravity-debian/"
+downurl="https://us-central1-apt.pkg.dev/projects/antigravity-auto-updater-dev/pool/antigravity-debian"
 depends=()
 lfs_depends=(bash coreutils glibc sed tar)
 blfs_depends=(libarchive 
 libx11 libxkbfile # Xorg libraries
 wget)
-filename=$(wget -cqO- $baseurl | grep deb | tail -n 1 | sed 's|File_name: pool/antigravity-debian/||g')
+filename=$(wget -cqO- $baseurl | grep deb | tail -n 1 | sed 's|File_name: pool/antigravity-debian/||g' | sed 's|Filename: pool/antigravity-debian/||g')
 version=$(echo $filename | grep -v "beta\|alpha\|rc" | cut -d '_' -f 2 | cut -d '-' -f 1)
-_STR=$(echo $filename | sed 's|antigravity_1.19.6-||g' | sed 's/.deb//g')
+_STR=$(echo $filename | sed "s|antigravity_$version-||g" | sed 's/.deb//g')
 
 if ! [[ -f $filename ]]; then
 	wget -c $downurl/$filename
@@ -18,7 +18,7 @@ fi
 sudo rm -rf $name
 mkdir $name
 cd $name
-bsdtar xf ../$filename
+bsdtar xf ../${filename}
 tar xf data.tar.xz
 cd usr/share
 sudo cp -r antigravity /usr/share
