b775c5f2af4ba39833506ae063ef53afff5155da
Brenton Horne <brentonhorne77@gmail.com>
2026-03-10 08:59:22 +1000

    spice-protocol: Fixing versioning

--- spice-protocol/build.sh
+++ spice-protocol/build.sh
@@ -25,7 +25,7 @@
 set -e
 # Variable declarations
 name=spice-protocol
-version=$(wget -cqO- https://spice-space.org/download/releases/ | grep "spice-protocol-.*xz\"" | grep -v "alpha\|beta\|rc" | cut -d '"' -f 8  | tail -n 1 | cut -d '-' -f 3 | sed 's/.tar.xz//g')
+version=$(wget -cqO- https://spice-space.org/download/releases/ | grep "spice-protocol-.*xz\"" | grep -v "alpha\|beta\|\.rc" | cut -d '"' -f 8  | tail -n 1 | cut -d '-' -f 3 | sed 's/.tar.xz//g')
 docs="COPYING *.md"
 direname="$name-$version"
 filename="$direname.tar.xz"
