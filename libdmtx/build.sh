95cb1eae1a083914e00dab409089e8e18700b1d1
Brenton Horne <brentonhorne77@gmail.com>
2026-03-14 22:16:24 +1000

    libdmtx: initial commit

---
+++ libdmtx/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+name=libdmtx
+version=$(wget -cqO- https://github.com/dmtx/libdmtx/tags | grep "/tag/v" | head -n 1 | cut -d '"' -f 6 | cut -d '/' -f 6 | sed 's/^v//g')
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.gz/}"
+depends=()
+lfs_depends=(glibc)
+blfs_depends=()
+
+if ! [[ -f $filename ]]; then
+	wget -c https://github.com/dmtx/libdmtx/archive/refs/tags/v${version}.tar.gz -O $filename
+fi
+
+tar xf $filename
+cd $direname
+autoreconf -vi
+./configure --prefix=/usr
+make -j$(nproc)
+sudo make install
+cd ..
+sudo rm -rf $filename $direname
+echo "$version" > /var/lib/lfs-custom-packages/$name
