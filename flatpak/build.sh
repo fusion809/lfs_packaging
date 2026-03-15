fbee6ffa91fec53f112b36acf7e64a16545421bb
Brenton Horne <brentonhorne77@gmail.com>
2026-03-16 02:33:51 +1000

    flatpak: Fixing version str; uses API approach, so imperfect, can timeout

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -26,7 +26,8 @@
 set -e
 # Variable declarations
 name=flatpak
-version=$(wget -cqO- https://github.com/flatpak/flatpak/releases/ | grep "/tag/" | grep -v "alpha\|beta\|rc" | head -n 1 | cut -d '"' -f 6 | cut -d '/' -f 6)
+#version=$(wget -cqO- https://github.com/flatpak/flatpak/releases/ | grep "/tag/" | grep -v "alpha\|beta\|rc" | head -n 1 | cut -d '"' -f 6 | cut -d '/' -f 6)
+version=$(curl -s 'https://api.github.com/repos/flatpak/flatpak/releases?per_page=30' | grep -E '"tag_name"|"prerelease"' | paste - - | grep 'false' | head -1 | grep -oP '(?<="tag_name": ")[^"]+')
 filename="$name-$version.tar.xz"
 direname="${filename/.tar.xz/}"
 depends=(gcab ostree)
