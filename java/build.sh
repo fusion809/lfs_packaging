32b2841d106c0c23d6dacc24f2d9c76dce67d6f3
Brenton Horne <brentonhorne77@gmail.com>
2026-03-16 04:07:43 +1000

    +java

---
+++ java/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+# Variable declaration
+name=java
+majorver=$(curl -s https://jdk.java.net/ | grep "Early access:" | cut -d '/' -f 2)
+minorver=$(curl -s https://jdk.java.net/$majorver/ | grep ">Build" | cut -d ' ' -f 2)
+version="$majorver+$minorver"
+filename="openjdk-$majorver-ea+${minorver}_linux-x64_bin.tar.gz"
+direname="jdk-$majorver"
+instdir="jdk-$version"
+# Fetch source and unpack it
+if ! [[ -f $filename ]]; then
+	wget -c https://download.java.net/java/early_access/jdk$majorver/$minorver/GPL/$filename
+fi
+rm -rf $direname
+tar xf $filename
+# Compile and install
+sudo rm -rf /opt/jdk-*
+sudo mv $direname /opt/$instdir
+sudo ln -sf /opt/$instdir /opt/jdk
+sudo chown root:root -R /opt/$instdir
+rm -rf $filename
+echo $version > /var/lib/custom-packages/$name
