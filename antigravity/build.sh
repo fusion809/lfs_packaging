42c8f6363be8ce9fd530983c9d0b2bda83d0fb4a
Brenton Horne <brentonhorne77@gmail.com>
2026-07-07 12:24:44 +1000

    Fixing antigravity

--- antigravity/build.sh
+++ antigravity/build.sh
@@ -14,8 +14,10 @@ direname="${filename/.tar.gz/}"
 if ! [[ -f $filename ]]; then
 	wget -c "https://edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/stable/$version-$_build/linux-x64/$filename"
 fi
+rm -rf "$direname"
 tar xf "$filename"
 sudo mkdir -p /usr/share/antigravity
+sudo ln -sf /usr/share/antigravity/bin/antigravity-ide /usr/bin/
 sudo cp -r "$direname"/* /usr/share/antigravity
 sudo cp $name.desktop /usr/share/applications/
 sudo cp $name.png /usr/share/pixmaps/
