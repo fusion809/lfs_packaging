4175c9fd6e710cdc2e538400be8da59fb0598cd1
Brenton Horne <brentonhorne77@gmail.com>
2026-07-07 12:06:11 +1000

    Tweaking antigravity versioning

--- antigravity/build.sh
+++ antigravity/build.sh
@@ -6,19 +6,17 @@ lfs_depends=(bash coreutils glibc sed tar)
 blfs_depends=(libarchive 
 libx11 libxkbfile # Xorg libraries
 wget)
-eval "$(curl -s "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=antigravity-ide" \
-  | grep -E '^(pkgver|_build)=' | sed 's|pkgver|version|g' \
-  | head -2)"
+version=$(wget -cqO- "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=antigravity-ide" | grep "^pkgver=" | sed 's/^pkgver=//g')
+_build=$(wget -cqO- "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=antigravity-ide" | grep "^_build=" | sed 's/^_build=//g')
 
-echo "filename=$filename"
-echo "direname=$direname"
+filename="Antigravity IDE.tar.gz"
+direname="${filename/.tar.gz/}"
 if ! [[ -f $filename ]]; then
 	wget -c "https://edgedl.me.gvt1.com/edgedl/release2/j0qc3/antigravity/stable/$version-$_build/linux-x64/$filename"
 fi
 tar xf "$filename"
 sudo mkdir -p /usr/share/antigravity
 sudo cp -r "$direname"/* /usr/share/antigravity
-sudo cp -r "$direname"/bin/* /usr/bin/
 sudo cp $name.desktop /usr/share/applications/
 sudo cp $name.png /usr/share/pixmaps/
 cd ..
