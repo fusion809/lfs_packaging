1b15017b1d80a5363b7bd31b78adf74a60111f2a
Brenton Horne <brentonhorne77@gmail.com>
2026-07-09 22:59:05 +1000

    Fixing builds

--- libX11/build.sh
+++ libX11/build.sh
@@ -2,7 +2,7 @@
 set -e
 # Variable declarations
 name=libX11
-version=$(wget -cqO- https://xorg.freedesktop.org/archive/individual/lib/ | grep "$name" | grep '\.tar\.xz"' | tail -n 1 | cut -d '-' -f 2 | cut -d '"' -f 1 | sed 's/.tar.xz//g')
+version=$(wget -cqO- https://xorg.freedesktop.org/archive/individual/lib/ | grep "$name-" | grep '\.tar\.xz"' | sed "s/$name-//g" | sed 's/\.tar\.xz//g' | cut -d '"' -f 2 | sort -V | tail -n 1)
 direname="${name}-$version"
 filename="$direname.tar.xz"
 lfs_depends=(bash coreutils glibc make sed systemd tar util-linux xz zlib)
--- libXi/build.sh
+++ libXi/build.sh
@@ -2,7 +2,7 @@
 set -e
 # Variable declarations
 name=libXi
-version=$(wget -cqO- https://xorg.freedesktop.org/archive/individual/lib/ | grep "$name" | grep '\.tar\.xz"' | tail -n 1 | cut -d '-' -f 2 | cut -d '"' -f 1 | sed 's/.tar.xz//g')
+version=$(wget -cqO- https://xorg.freedesktop.org/archive/individual/lib/ | grep "$name-" | grep '\.tar\.xz"' | tail -n 1 | cut -d '-' -f 2 | cut -d '"' -f 1 | sed 's/.tar.xz//g')
 direname="${name}-$version"
 filename="$direname.tar.xz"
 lfs_depends=(bash coreutils glibc make sed systemd tar util-linux xz zlib)
