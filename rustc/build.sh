af3a6eb72a0a4dda776bb7e45fec95c76f3f7ebf
Brenton Horne <brentonhorne77@gmail.com>
2026-07-10 12:07:07 +1000

    +rustc

---
+++ rustc/build.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+set -e
+name=rustc
+version=$(wget -cqO- https://blog.rust-lang.org/releases/latest | grep "\-[0-9]\." | head -n 1 | cut -d '/' -f 5 | cut -d '-' -f 2)
+filename="$name-$version-src.tar.xz"
+direname="${filename/.tar.xz/}"
+ssl_src="https://github.com/lfs-book/rust-openssl/archive/v0.10.78/rust-openssl-0.10.78.tar.gz"
+depends=()
+lfs_depends=(coreutils glibc python)
+blfs_depends=(llvm cmake curl)
+if ! [[ -f $filename ]]; then
+	wget -c https://static.rust-lang.org/dist/$filename
+	wget -c $ssl_src
+fi
+
+tar xf $filename
+cd $direname
+sudo mkdir -pv /opt/$name-$version      &&
+sudo ln -svfn $name-$version /opt/$name
+cat << EOF > bootstrap.toml
+# See bootstrap.toml.example for more possible options,
+# and see src/bootstrap/defaults/bootstrap.dist.toml for a few options
+# automatically set when building from a release tarball
+# (unfortunately, we have to override many of them).
+
+# Tell x.py that the editors have reviewed the content of this file
+# and updated it to follow the major changes of the building system,
+# so x.py will not warn users to review that information.
+change-id = 154508
+
+[llvm]
+# When using the system installed copy of LLVM, prefer the shared libraries
+link-shared = true
+
+# If building the shipped LLVM source, only enable the x86 target
+# instead of all the targets supported by LLVM.
+targets = "X86"
+
+[build]
+description = "for BLFS r13.0-1024"
+
+# Omit the documentation to save time and space (the default is to build them).
+docs = false
+
+# Only install these extended tools. Cargo, clippy, rustdoc, and rustfmt
+# are installed by a default rustup installation, and rust-src is needed
+# to build the Rust code in Linux kernel (in case you need such a kernel
+# feature).
+tools = ["cargo", "clippy", "rustdoc", "rustfmt", "src"]
+
+[install]
+prefix = "/opt/rustc-$version"
+docdir = "share/doc/rustc-$version"
+
+[rust]
+channel = "stable"
+
+# Enable the same optimizations as the official upstream build.
+lto = "thin"
+codegen-units = 1
+
+# Don't build llvm-bitcode-linker which is only useful for the NVPTX
+# backend that we don't enable.
+llvm-bitcode-linker = false
+
+[target.x86_64-unknown-linux-gnu]
+llvm-config = "/usr/bin/llvm-config"
+
+[target.i686-unknown-linux-gnu]
+llvm-config = "/usr/bin/llvm-config"
+EOF
+tar xf ../rust-openssl-0.10.78.tar.gz &&
+
+cat >> src/tools/cargo/Cargo.toml << EOF &&
+[patch.crates-io]
+openssl = { path = "../../../rust-openssl-0.10.78/openssl" }
+openssl-sys = { path = "../../../rust-openssl-0.10.78/openssl-sys" }
+EOF
+
+sed -ri src/tools/cargo/Cargo.lock \
+    -e '/name = "openssl-sys"/,/^$/{/source|checksum/d;s/0.9.112/0.9.114/}' \
+    -e '/name = "openssl"/,/^$/{/source|checksum/d;s/0.10.76/0.10.78/}'     \
+    -e '/name = "openssl-macros"/,/^$/{/source|checksum/d}'
+export LIBSSH2_SYS_USE_PKG_CONFIG=1
+export LIBSQLITE3_SYS_USE_PKG_CONFIG=1
+./x.py build
+sudo ./x.py install
+sudo rm -fv /opt/rustc-$version/share/doc/rustc-$version/*.old   &&
+sudo install -vm644 README.md                                \
+               /opt/rustc-$version/share/doc/rustc-$version &&
+
+sudo install -vdm755 /usr/share/zsh/site-functions      &&
+sudo ln -sfv /opt/rustc/share/zsh/site-functions/_cargo \
+        /usr/share/zsh/site-functions
+
+sudo mv -v /etc/bash_completion.d/cargo /usr/share/bash-completion/completions
+unset LIB{SSH2,SQLITE3}_SYS_USE_PKG_CONFIG
+echo $version >> /var/lib/custom-packages/$name
+sudo find /opt -maxdepth 1 -type d -name 'rustc-*' ! -name "rustc-$version" -exec sudo rm -rf {} +
