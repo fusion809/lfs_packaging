55281f5eeee78d8c74b70ebdff00fa7ad8406b5e
Brenton Horne <brentonhorne77@gmail.com>
2026-07-12 16:15:13 +1000

    Tweakks to executor

--- executor/build.sh
+++ executor/build.sh
@@ -4,8 +4,12 @@ version=$(wget -cqO- https://raw.githubusercontent.com/fusion809/executor-raujon
 lfs_depends=(wget)
 blfs_depends=(gnome-shell gnome-shell-extensions glib2 git)
 
-if ! [[ -d /usr/share/gnome-shell/extensions/executor-raujonas.github.ip ]]; then
+if ! [[ -d /usr/share/gnome-shell/extensions/executor@raujonas.github.io ]]; then
 	sudo git -C /usr/share/gnome-shell/extensions clone https://github.com/fusion809/executor-raujonas.github.io executor@raujonas.github.io
+else
+	sudo git -C /usr/share/gnome-shell/extensions/executor@raujonas.github.io pull origin master
 fi
-sudo glib-compile-schemas /usr/share/gnome-shell/extensions/executor@raujonas.github.io/schemas
+sudo chmod 777 -R /usr/share/gnome-shell/extensions/executor@raujonas.github.io
+glib-compile-schemas /usr/share/gnome-shell/extensions/executor@raujonas.github.io/schemas
+dconf load /org/gnome/shell/extensions/executor/ < /usr/share/gnome-shell/extensions/executor@raujonas.github.io/executor-settings.dconf
 echo "$version" > /var/lib/custom-packages/$name
