cf9d03069f73817105940cd785c2f7df115e8ce5
Brenton Horne <brentonhorne77@gmail.com>
2026-07-25 00:33:48 +1000

    +appstream-glib, pango

---
+++ appstream-glib/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+# Variable declarations
+name=appstream-glib
+version=$(wget -cqO- https://people.freedesktop.org/~hughsient/appstream-glib/releases/ | grep "appstream-glib-.*.tar.xz" | tail -n 1 | cut -d '"' -f 2 | sed 's/appstream-glib-//g' | sed 's/.tar.xz//g')
+depends=()
+blfs_depends=(curl gdk-pixbuf gtk3 json-glib libarchive libyaml gtk-doc)
+pip_depends=()
+direname="$name-$version"
+filename="$direname.tar.xz"
+# Fetch and unpack source
+rm -rf $direname
+if ! [[ -f $filename ]]; then
+	wget -c http://people.freedesktop.org/~hughsient/appstream-glib/releases/$filename
+fi
+tar xvf $filename
+# Compile and install
+cd $direname
+CFLAGS="-O2 -fPIC"
+CXXFLAGS="-O2 -fPIC"
+# sed no longer needed for 1.1.4+ (xsl-ns -> xsl change was for older versions)
+mkdir build
+cd build
+meson setup --prefix=/usr            \
+            --buildtype=release      \
+            -D rpm=false         \
+            -D man=false        .. &&
+ninja -j$(nproc)
+sudo ninja install
+# Cleanup and add to database
+cd ..
+rm -rf $filename $direname
+echo $version > /var/lib/custom-packages/$name
---
+++ pango/build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+# Variable declaration
+name=pango
+version="$(wget -cqO- https://gitlab.gnome.org/GNOME/$name/-/tags | grep "tags/" | cut -d '/' -f 6 | sed 's/".*//g' | grep -v "alpha\|beta\|\.rc" | grep -v "1.90" | head -n 1 | sed 's/^v//g')"
+filename="$name-$version.tar.bz2"
+direname="${filename/.tar.bz2/}"
+blfs_depends=(fontconfig harfbuzz freetype fribidi glib2 cairo)
+depends=(xorg-libs)
+# Fetch source and unpack it
+if ! [[ -f $filename ]]; then
+	wget -c https://gitlab.gnome.org/GNOME/$name/-/archive/$version/$filename
+fi
+rm -rf "$direname"
+tar xf $filename
+# Compile and install
+cd "$direname"
+CFLAGS="-O2 -fPIC"
+CXXFLAGS="-O2 -fPIC"
+mkdir build &&
+cd    build &&
+
+meson setup --prefix=/usr            \
+            --buildtype=release      \
+            --wrap-mode=nofallback   \
+            -D introspection=enabled \
+            ..                       &&
+ninja -j$(nproc)
+sudo ninja install
+# Cleanup and add to database
+cd ../..
+sudo rm -rf $direname $filename
+echo $version > /var/lib/custom-packages/$name
