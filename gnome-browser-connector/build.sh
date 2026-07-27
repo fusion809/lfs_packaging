9ac047653aac8b86e873d23bb3a3814c61646021
Brenton Horne <brentonhorne77@gmail.com>
2026-07-27 20:57:37 +1000

    +libarchive to gnome-browser-connector extension

--- gnome-browser-connector/build.sh
+++ gnome-browser-connector/build.sh
@@ -3,6 +3,9 @@ name=gnome-browser-connector
 version=$(git ls-remote --tags --refs https://gitlab.gnome.org/GNOME/gnome-browser-connector.git | awk '{print $2}' | sed 's|refs/tags/||' | sed 's/^v//g' | sort -V | tail -n1)
 lfs_depends=(python meson)
 blfs_depends=(pygobject gnome-shell glib2 git)
+depends=(libarchive) # Required to actually install extensions
+# libarchive provided by BLFS doesn't work due to bsdunzip incompatibility with
+# info-zip unzip commands
 if ! [[ -d $name ]]; then
 	git clone https://gitlab.gnome.org/GNOME/gnome-browser-connector
 fi
