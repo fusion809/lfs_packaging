ede5a2b2469489638371327b75bd05d03f95849a
Brenton Horne <brentonhorne77@gmail.com>
2026-08-05 16:53:31 +1000

    elfutils: improving reliability of version line

--- elfutils/build.sh
+++ elfutils/build.sh
@@ -2,7 +2,9 @@
 set -e
 name=elfutils
 #version=$(git ls-remote --tags https://sourceware.org/git/elfutils.git | grep -oP 'refs/tags/elfutils-\K[0-9.]+$' | sort -V | tail -n 1)
-version=$(wget -cqO- https://sourceware.org/elfutils/ftp/ | grep -oE 'href="[0-9.]+/"' | sed -E 's/href="([^/]+)\/"/\1/' | sort -V | tail -n 1)
+sw_ver=$(wget -cqO- https://sourceware.org/elfutils/ftp/ | grep -oE 'href="[0-9.]+/"' | sed -E 's/href="([^/]+)\/"/\1/' | sort -V | tail -n 1)
+arch_ver=$(wget -cqO- -T 10 "https://gitlab.archlinux.org/archlinux/packaging/packages/$name/-/raw/main/PKGBUILD" | grep "^pkgver=" | cut -d '=' -f 2)
+version=${sw_ver:-$arch_ver}
 filename="$name-$version.tar.bz2"
 direname="${filename/.tar.bz2/}"
 lfs_depends=(bzip2
