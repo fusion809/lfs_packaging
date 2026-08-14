730826efbd07bfc7b83d7e022b25c87eda3b200e
Brenton Horne <brentonhorne77@gmail.com>
2026-08-14 22:24:35 +1000

    Rm comments around deps

--- gnome-browser-connector/build.sh
+++ gnome-browser-connector/build.sh
@@ -3,9 +3,7 @@ name=gnome-browser-connector
 version=$(gn_ver $name)
 lfs_depends=(python meson)
 blfs_depends=(pygobject gnome-shell glib2 git)
-depends=(libarchive) # Required to actually install extensions
-# libarchive provided by BLFS doesn't work due to bsdunzip incompatibility with
-# info-zip unzip commands
+depends=(libarchive)
 if ! [[ -d $name ]]; then
 	git clone https://gitlab.gnome.org/GNOME/gnome-browser-connector
 fi
