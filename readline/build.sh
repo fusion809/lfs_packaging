abd21f0b7be7579e2eabe42a3ce8301b8eac2011
Brenton Horne <brentonhorne77@gmail.com>
2026-08-29 15:41:27 +1000

    +readline

---
+++ readline/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+name=readline
+version=$(gnu_ver $name)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+lfs_depends=(gcc glibc make ncurses tar wget xz)
+if ! [[ -f $filename ]]; then
+    wget -c https://ftpmirror.gnu.org/$name/$filename
+fi
+rm -rf $direname
+tar xf $filename
+cd $direname
+sed -i '/MV.*old/d' Makefile.in
+sed -i '/{OLDSUFF}/c:' support/shlib-install
+sed -i 's/-Wl,-rpath,[^ ]*//' support/shobj-conf
+sed -e '270a\
+     else\
+       chars_avail = 1;'      \
+    -e '288i\   result = -1;' \
+    -i.orig input.c
+./configure --prefix=/usr    \
+            --disable-static \
+            --with-curses    \
+            --docdir=/usr/share/doc/$direname
+make SHLIB_LIBS="-lncursesw"
+sudo make install
+sudo install -v -m644 doc/*.{ps,pdf,html,dvi} /usr/share/doc/$direname
+cd ..
+rm -rf $filename $direname
+echo "$version" > /var/lib/custom-packages/$name
