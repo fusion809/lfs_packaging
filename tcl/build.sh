7d02d2d875622110de8a584bd2840ff3a7738db7
Brenton Horne <brentonhorne77@gmail.com>
2026-08-30 15:22:10 +1000

    +tzdata

---
+++ tcl/build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -e
+name=tcl
+get_version() {
+	local inst_ver=$(pkgver $name)
+	local up_ver=$(wget -T 5 -t 1 -c https://github.com/tcltk/tcl/releases -qO-  | grep "core-8" | sed 's/.*core-//g' | cut -d '"' -f 1 | sed 's/&amp.*//g' | tr '-' '.' | sort -V | tail -n 1)
+	ver_check "$up_ver" "$inst_ver" && return
+	local git_ver=$(timeout 5 git ls-remote --tags --refs https://github.com/tcltk/tcl.git | grep -E "core-8-[0-9]-[0-9]+$" | cut -d '/' -f 3 | sed 's/core-//g' | tr '-' '.' | sort -V | tail -n 1)
+	ver_check "$git_ver" "$inst_ver" && return
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" && return
+	local lfs_vers=$(lfs_ver $name)
+	ver_check "$lfs_vers" "$inst_ver" && return
+	fver "$name" "$inst_ver"
+}
+version=$(get_version)
+lfs_depends=(zlib gcc make tar gzip coreutils bash)
+filename="${name}${version}-src.tar.gz"
+docs_filename="${name}${version}-html.tar.gz"
+direname="${filename/-src.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://sourceforge.net/projects/tcl/files/Tcl/$version/$filename
+fi
+if ! [[ -f $docs_filename ]]; then
+	wget -c https://sourceforge.net/projects/tcl/files/Tcl/$version/$docs_filename
+fi
+sudo rm -rf $direname
+tar xf $filename
+cd $direname
+SRCDIR=$(pwd)
+cd unix
+./configure --prefix=/usr           \
+            --mandir=/usr/share/man \
+            --disable-rpath
+make -j$(nproc)
+
+sed -e "s|$SRCDIR/unix|/usr/lib|" \
+    -e "s|$SRCDIR|/usr/include|"  \
+    -i tclConfig.sh
+
+basever=$(echo $version | sed -E 's/\.[0-9]+$//'g)
+sed -e "s|$SRCDIR/unix/pkgs/tdbc|/usr/lib/tdbc|" \
+    -e "s|$SRCDIR/pkgs/tdbc[0-9.]+/generic|/usr/include|"     \
+    -e "s|$SRCDIR/pkgs/tdbc[0-9.]+/library|/usr/lib/tcl$basever|"  \
+    -e "s|$SRCDIR/pkgs/tdbc[0-9.]+|/usr/include|"             \
+    -i pkgs/tdbc*/tdbcConfig.sh
+
+sed -e "s|$SRCDIR/unix/pkgs/itcl|/usr/lib/itcl|" \
+    -e "s|$SRCDIR/pkgs/itcl[0-9.]+/generic|/usr/include|"    \
+    -e "s|$SRCDIR/pkgs/itcl[0-9.]+|/usr/include|"            \
+    -i pkgs/itcl*/itclConfig.sh
+
+unset SRCDIR
+sudo su -c "make install 
+chmod 644 /usr/lib/libtclstub$basever.a
+chmod -v u+w /usr/lib/libtcl$basever.so
+make install-private-headers
+ln -sfv tclsh$basever /usr/bin/tclsh"
+cd ..
+sudo su -c "tar -xf ../$docs_filename --strip-components=1
+mkdir -v -p /usr/share/doc/$name-$version
+cp -v -r  ./html/* /usr/share/doc/$name-$version"
+cd ..
+sudo rm -rf $filename $direname $docs_filename
+echo "$version" | sudo tee /var/lib/custom-packages/$name
---
+++ tzdata/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+name=tzdata
+_timezones=('africa' 'antarctica' 'asia' 'australasia'
+           'europe' 'northamerica' 'southamerica'
+           'etcetera' 'backward' 'factory')
+get_version() {
+	local inst_ver=$(pkgver $name)
+	local up_ver=$(wget -T 5 -t 1 -cqO- https://www.iana.org/time-zones | grep "time-zones/releases" | cut -d '/' -f 4 | sed 's/".*//g' | head -n 1)
+	ver_check "$up_ver" "$inst_ver" && return
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" && return
+	fver "$name" "$inst_ver"
+}
+version=$(get_version)
+filename="tzdata${version}.tar.gz"
+direname="tzdata${version}"
+if ! [[ -f $filename ]]; then
+	wget -c https://www.iana.org/time-zones/repository/releases/$filename
+fi
+rm -rf $direname
+mkdir $direname
+tar xf $filename -C $direname
+cd $direname
+ZONEINFO=/usr/share/zoneinfo
+mkdir -pv $ZONEINFO/{posix,right}
+for tz in etcetera southamerica northamerica europe africa antarctica \
+ asia australasia backward; do
+ sudo zic -L /dev/null -d $ZONEINFO ${tz}
+ sudo zic -L /dev/null -d $ZONEINFO/posix ${tz}
+ sudo zic -L leapseconds -d $ZONEINFO/right ${tz}
+done
+sudo cp -v zone.tab zone1970.tab iso3166.tab $ZONEINFO
+sudo zic -d $ZONEINFO -p America/New_York
+unset ZONEINFO tz
+cd ..
+rm -rf $filename $direname
+echo "$version" > /var/lib/custom-packages/$name
+
