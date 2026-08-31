2b939ef5f87318544a8e03c41e53b14cee9ad325
Brenton Horne <brentonhorne77@gmail.com>
2026-08-31 16:16:41 +1000

    Quote psmisc/psmisc in psmisc

--- psmisc/build.sh
+++ psmisc/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 name=psmisc
-version=$(gl_ver $name/$name)
+version=$(gl_ver "$name/$name")
 filename="$name-$version.tar.xz"
 direname="${filename/.tar.*/}"
 lfs_depends=(ncurses make gcc tar xz coreutils)
