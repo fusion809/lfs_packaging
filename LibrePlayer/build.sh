1cfb4d5f61757464f6db285733bedbaa385dbfcd
Brenton Horne <brentonhorne77@gmail.com>
2026-09-02 09:22:21 +1000

    Libre_Player->LibrePlayer; git->tarball

--- LibrePlayer/build.sh
+++ LibrePlayer/build.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 set -e
-name=Libre_Player
-version=$(gh_com "Procurador1337/$name")
-direname=$name
+name=LibrePlayer
+repo="Procurador1337/$name"
+version=$(gh_com "$repo")
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
 blfs_depends=(brotli cmake double-conversion flac fontconfig freetype graphite2 harfbuzz keyutils lame libXau libXdmcp libdrm libogg libpng libsndfile libvorbis libxcb libxkbcommon libxml2 llvm lm-sensors mpg123 opus pulseaudio qt6 spirv-tools)
 lfs_depends=(bzip2 dbus e2fsprogs expat gcc glibc libelf libffi openssl systemd xz zlib zstd)
 depends=(glib2 libX11 libXext libXxf86vm libpciaccess libxshmfence mesa mitkrb pcre2 wayland)
 
-if ! [[ -d $direname/.git ]]; then
-	git clone https://github.com/Procurador1337/$name
+if ! [[ -f $filename ]]; then
+	wget -c https://github.com/$repo/archive/$version.tar.gz -O $filename
 fi
-
+rm -rf $direname
+tar xf $filename
 cd $direname
-git stash
-git pull origin main
 common_cmake_args=(
   -DCMAKE_BUILD_TYPE=None
   -DCMAKE_INSTALL_PREFIX=/usr
   -Wno-dev
 )
 cmaki "${common_cmake_args[@]}"
-cd ..
-rm -rf build
-cd ..
+cd ../..
+rm -rf $filename $direname
 echo "$version" > /var/lib/custom-packages/$name
