3d3e056851566e5ae60fc68e408d8266c1eb4d4a
Brenton Horne <brentonhorne77@gmail.com>
2026-09-05 03:49:56 +1000

    Fixing icu build

--- icu/build.sh
+++ icu/build.sh
@@ -4,13 +4,13 @@ name=icu
 repo="unicode-org/$name"
 version=$(gh_ver $repo)
 filename="${name}4c-$version-sources.tgz"
-direname="${filename/.tar.*/}"
+direname="${filename/.tgz/}"
 if ! [[ -f $filename ]]; then
 	wget -c https://github.com/$repo/releases/download/release-$version/$filename
 fi
 rm -rf "$direname"
 tar xf "$filename"
-cd "$direname/source"
+cd "icu/source"
 cmi --prefix=/usr
 cd ../..
 rm -rf "$filename" "$direname"
