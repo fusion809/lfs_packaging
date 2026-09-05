710163737bf40328dd6137142775d334dd8b0bbd
Brenton Horne <brentonhorne77@gmail.com>
2026-09-05 17:29:10 +1000

    +deps for libcloudproviders

--- libcloudproviders/build.sh
+++ libcloudproviders/build.sh
@@ -5,7 +5,7 @@ version=$(gn_ver libcloudproviders)
 majVer=$(echo $version | sed -E 's/.[0-9]+$//g')
 filename="$name-$version.tar.xz"
 direname="${filename/.tar.*/}"
-depends=(glib2)
+depends=(glib2 glibc libffi pcre2 systemd util-linux zlib)
 if ! [[ -f $filename ]]; then
 	wget -c https://download.gnome.org/sources/libcloudproviders/$majVer/$filename
 fi
