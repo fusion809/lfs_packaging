18e52f1564aa8fc9735d103f2b514952b9e8cd50
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 07:48:00 +1000

    +lm-sensors

---
+++ lm-sensors/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+name=lm-sensors
+repo=hramrach/$name
+version=$(gh_ver $repo | sed 's/-/./g')
+depends=(glibc)
+filename="$name-${version/./-/}.tar.gz"
+direname="${filename/.tar.*/}"
+# Kernel config options required, too
+if ! [[ -f $filename ]]; then
+	wget -c https://github.com/$repo/archive/V${version/./-}/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+make -j$(nproc) PREFIX=/usr        \
+     BUILD_STATIC_LIB=0 \
+     MANDIR=/usr/share/man
+sudo su -c "make PREFIX=/usr        \
+     BUILD_STATIC_LIB=0 \
+     MANDIR=/usr/share/man install &&
+
+install -v -m755 -d /usr/share/doc/lm-sensors-${version/./-} &&
+cp -rv              README INSTALL doc/* \
+                    /usr/share/doc/lm-sensors-${version/./-}"
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
