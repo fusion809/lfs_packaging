b3d589cba17dcf23f53e3e3dd50bc93b75425b28
Brenton Horne <brentonhorne77@gmail.com>
2026-09-08 13:31:48 +1000

    +libXau

---
+++ libXau/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+name=libXau
+version=$(xfd_ver $name)
+depends=(glibc)
+filename="$name-$version.tar.xz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://www.x.org/pub/individual/lib/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+cmi --prefix=/usr
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
