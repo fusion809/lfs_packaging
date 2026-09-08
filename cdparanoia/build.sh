92670335434be877c652da6670c31ef4d6405520
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 07:43:03 +1000

    +cdparanoia

---
+++ cdparanoia/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+name=cdparanoia
+repo=jwilk-mirrors/$name
+version=$(gh_ver $repo)
+depends=(glibc)
+filename="$name-III-$version.src.tgz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://downloads.xiph.org/releases/cdparanoia/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+gap_patches "$name"
+./configure --prefix=/usr --mandir=/usr/share/man
+make -j1
+sudo make install
+sudo su -c "chmod -v 755 /usr/lib/libcdda_*.so.0.$version &&
+rm -fv /usr/lib/libcdda_*.a"
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
