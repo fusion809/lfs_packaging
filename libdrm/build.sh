bcf746b1f190b9d6c11ed39bc2787ce1ed579210
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 08:45:21 +1000

    +libdrm

---
+++ libdrm/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+name=libdrm
+repo=Distrotech/$name
+version=$(gh_ver $repo)
+depends=(glibc libpciaccess zlib)
+filename="$name-$version.tar.xz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://dri.freedesktop.org/libdrm/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+options=(--prefix=/usr \
+            --buildtype=release   \
+            -D udev=true          \
+	    -D valgrind=disabled)
+mni "${options[@]}"
+cd ../..
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
