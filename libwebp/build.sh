e3f989baef2f33c4d3a10b48fab47725fe2127ee
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 07:39:36 +1000

    libwebp: Fixing versioning

--- libwebp/build.sh
+++ libwebp/build.sh
@@ -4,7 +4,7 @@ name=libwebp
 repo=webmproject/$name
 get_version() {
 	local inst_ver=$(pkgver $name)
-	local up_ver=$(wget -T 5 -t 1 -cqO- https://github.com/webmproject/libwebp/tags/ | grep "v1\.[0-9]\.[0-9]" -oE | sort -V | tail -n 1)
+	local up_ver=$(wget -T 5 -t 1 -cqO- https://github.com/webmproject/libwebp/tags/ | grep "v1\.[0-9]\.[0-9]" -oE | sed 's/^v//g' | sort -V | tail -n 1)
 	ver_check "$up_ver" "$inst_ver" && return
 	local git_ver=$(timeout 5 git ls-remote --tags --refs https://github.com/webmproject/libwebp.git | grep "refs/tags/v[0-9.]+" -oE | sed 's/.*v//g' | sort -V | tail -n 1)
 	ver_check "$git_ver" "$inst_ver" && return
