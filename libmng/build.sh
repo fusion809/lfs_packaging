a94e0606bb825e947620629df77b46a0628ab5c1
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 09:53:33 +1000

    +libmng

---
+++ libmng/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+name=libmng
+repo=LuaDist/$name
+version=$(gh_ver $repo)
+depends=(glibc lcms2 libjpeg-turbo zlib)
+filename="$name-$version.tar.xz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://downloads.sourceforge.net/libmng/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+cmi --prefix=/usr --disable-static
+sudo su -c "install -v -m755 -d        /usr/share/doc/$direname &&
+install -v -m644 doc/*.txt /usr/share/doc/$direname"
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
