61df027ea9a9b904db6061d5629aa5d01273206a
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 09:42:16 +1000

    +xdotool

---
+++ xdotool/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+name=xdotool
+repo=jordansissel/$name
+version=$(gh_ver $repo)
+depends=(glibc libX11 libXau libXdmcp libXext libXi libXinerama libXtst libxcb libxkbcommon)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://github.com/$repo/archive/v$version/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+make WITHOUT_RPATH_FIX=1 -j$(nproc)
+sudo make PREFIX=/usr INSTALLMAN=/usr/share/man install
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
