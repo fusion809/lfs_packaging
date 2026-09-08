1e5e029797aaaae454454c556ebf8a2a8b9a4d0d
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 08:23:27 +1000

    +libogg

---
+++ libogg/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+name=libogg
+repo=gcp/$name
+version=$(gh_ver $repo)
+depends=(glibc)
+filename="$name-$version.tar.xz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://downloads.xiph.org/releases/ogg/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+cmi --prefix=/usr --disable-static --docdir=/usr/share/doc/$direname
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
