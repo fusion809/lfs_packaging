6c5631dcef24c4f04eaa304f966c14cec56fc648
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 08:10:53 +1000

    +lcms2

---
+++ lcms2/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+name=lcms2
+repo=mm2/Little-CMS
+version=$(gh_ver $repo)
+depends=(glibc libjpeg-turbo libwebp tiff xz zlib zstd)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://github.com/$repo/releases/download/${name}$version/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+mni --prefix=/usr --buildtype=release
+cd ../..
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
