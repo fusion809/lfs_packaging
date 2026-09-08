f6b5565a79893469a3de3d7cc2ccd733b17d8f6c
Brenton Horne <brentonhorne77@gmail.com>
2026-09-08 10:07:29 +1000

    +xorg-server

--- dolphin-plugins/build.sh
+++ dolphin-plugins/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-name=dolphin
+name=dolphin-plugins
 repo=KDE/$name
 version=$(gh_ver $repo)
-depends=(acl attica attr brotli bzip2 double-conversion e2fsprogs expat fontconfig freetype gcc glib2 glibc graphite2 harfbuzz icu karchive kbookmarks kcmutils kcodecs kcolorscheme kcompletion kconfig kconfigwidgets kcoreaddons kcrash kdbusaddons keyutils kfilemetadata kglobalaccel kguiaddons ki18n kiconthemes kio kitemviews kjobwidgets knewstuff knotifications kpackage kparts kservice ktextwidgets kuserfeedback kwidgetsaddons kwindowsystem kxmlgui libX11 libXext libXfixes libXxf86vm libffi libpciaccess libpng libxkbcommon libxml2 libxshmfence mesa mitkrb openssl pcre2 solid sonnet syndication systemd util-linux wayland webkitgtk xz zlib zstd)
+depends=(acl attr brotli bzip2 dolphin double-conversion e2fsprogs expat fontconfig freetype gcc glib2 glibc graphite2 harfbuzz icu karchive kauth kbookmarks kcodecs kcolorscheme kcompletion kconfig kconfigwidgets kcoreaddons kcrash keyutils kglobalaccel kguiaddons ki18n kiconthemes kio kitemviews kjobwidgets knotifications kparts kservice ktexteditor ktextwidgets kwidgetsaddons kwindowsystem kxmlgui libX11 libXext libXfixes libXxf86vm libffi libpciaccess libpng libxkbcommon libxml2 libxshmfence mesa mitkrb openssl pcre2 solid sonnet syntax-highlighting systemd util-linux wayland webkitgtk xz zlib zstd)
 blfs_depends=(breeze-icons flac lame libXau libXdmcp libcanberra libdrm libogg libsndfile libvorbis libxcb llvm lm-sensors mpg123 opus pulseaudio qt6 spirv-tools xcb-util-keysyms)
 lfs_depends=(dbus libelf)
 majVer=$(echo $version | sed -E 's/\.[0-9]+$//g')
---
+++ xorg-server/build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+name=xorg-server
+repo=$name/$name
+get_version() {
+	local inst_ver=$(pkgver $name)
+	local up_ver=$(wget -T 5 -t 1 -cqO- https://www.x.org/pub/individual/xserver/ | grep "xorg-server-[0-9]+\.[0-9]+\.[0-9]+" -oE | grep -v "\.99" | sed 's/xorg-server-//g' | sort -V | tail -n 1)
+	ver_check "$up_ver" "$inst_ver" && return
+	local git_ver=$(timeout 5 git ls-remote --tags --refs https://gitlab.freedesktop.org/xorg/xserver.git | grep "refs/tags/xorg-server-[0-9]+\.[0-9]+\.[0-9]+" -oE | sed 's/.*-//g' | grep -v ".99" | sort -V | tail -n 1)
+	ver_check "$git_ver" "$inst_ver" && return
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" && return
+	local lfs_vers=$(lfs_ver $name)
+	ver_check "$lfs_vers" "$inst_ver" && return
+	fver "$name" "$inst_ver"
+}
+version=$(get_version)
+depends=(brotli bzip2 expat freetype gcc glibc icu libX11 libXext libXfont2 libXxf86vm libepoxy libffi libfontenc libpciaccess libpng libxml2 libxshmfence mesa nettle systemd xz zlib zstd)
+blfs_depends=(libXau libXdmcp libdrm libtirpc libxcb libxcvt llvm lm-sensors pixman spirv-tools)
+lfs_depends=(dbus libelf)
+filename="$name-$version.tar.xz"
+direname="${filename/.tar.*/}"
+# Kernel config options required
+if ! [[ -f $filename ]]; then
+	wget -c https://www.x.org/pub/individual/xserver/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+gap_patches $name
+meson_options=(--prefix=/usr --localstatedir=/var -D glamor=true -D xkb_output_dir=/var/lib/xkb)
+mni "${meson_options[@]}"
+sudo mkdir -pv /etc/X11/xorg.conf.d
+cd ../..
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
