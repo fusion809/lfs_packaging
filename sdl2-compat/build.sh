b5073b02baa081480fe5a5654b215577c5fb1fbd
Brenton Horne <brentonhorne77@gmail.com>
2026-09-08 12:07:15 +1000

    +sdl2-compat

---
+++ sdl2-compat/build.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+name=sdl2-compat
+get_version() {
+	local inst_ver=$(pkgver $name)
+	local up_ver=$(wget -T 5 -t 1 -cqO- https://www.libsdl.org/release | grep "sdl2-compat-[0-9]\.[0-9]+\.[0-9]+" -oE | sed 's/sdl2-compat-//g' | sort -V | tail -n 1
+	ver_check "$up_ver" "$inst_ver" && return
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" && return
+	local lfs_vers=$(lfs_ver $name)
+	ver_check "$lfs_vers" "$inst_ver" && return
+	fver "$name" "$inst_ver"
+}
+version=$(get_version)
+depends=(glibc)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://www.libsdl.org/release/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+cmake_options=(-D CMAKE_INSTALL_PREFIX=/usr -D CMAKE_BUILD_TYPE=Release -D CMAKE_SKIP_INSTALL_RPATH=ON \
+      -D SDL2COMPAT_STATIC=OFF       \
+      -D SDL2COMPAT_TESTS=OFF        \
+      -W no-author -G Ninja)
+cmaki "${cmake_options[@]}"
+sudo rm -vf /usr/lib/libSDL2_test.a
+cd ../..
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
