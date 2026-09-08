4fea6c23cfc6e459f8379ccfebb835e7f604cfd6
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 09:22:37 +1000

    +soundtouch

---
+++ soundtouch/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+name=soundtouch
+repo=VinMing/$name
+version=$(gh_ver $repo)
+depends=(gcc glibc)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://www.surina.net/soundtouch/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+unset ACLOCAL
+./bootstrap
+cmi --prefix=/usr --docdir=/usr/share/doc/$direname
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
