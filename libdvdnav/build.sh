3875d8ca4d364f0b4bbba556ea923758b16f98f6
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 08:51:43 +1000

    +libdvdnav

---
+++ libdvdnav/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+name=libdvdread
+get_version() {
+	local inst_ver=$(pkgver $name)
+	local git_ver=$(timeout 5 git ls-remote --tags --refs https://code.videolan.org/videolan/$name.git | cut -d '/' -f 3 | sort -V | tail -n 1)
+	ver_check "$git_ver" "$inst_ver" && return
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" && return
+	local lfs_vers=$(lfs_ver $name)
+	ver_check "$lfs_vers" "$inst_ver" && return
+	fver "$name" "$inst_ver"
+}
+version=$(get_version)
+depends=(glibc)
+filename="$name-$version.tar.xz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://get.videolan.org/$name/$version/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+sed -i "/get_option/s/$name/&-$version/" meson.build
+options=(--prefix=/usr       \
+            --buildtype=release \
+	    --default-library=shared)
+mni "${options[@]}"
+cd ../..
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
