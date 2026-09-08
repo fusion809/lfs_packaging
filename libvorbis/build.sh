e09f77c37cc603b9c00376781d3bc3d650985595
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 08:32:19 +1000

    +libvorbis

---
+++ libvorbis/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+name=libvorbis
+get_version() {
+	local inst_ver=$(pkgver $name)
+	local up_ver=$(wget -T 5 -t 1 -cqO- https://gitlab.xiph.org/xiph/vorbis/-/tags | grep "v[0-9]+\.[0-9]+\.[0-9]+" -oE | sed 's/^v//g' | sort -V | tail -n 1)
+	ver_check "$up_ver" "$inst_ver" && return
+	local git_ver=$(timeout 5 git ls-remote --tags --refs https://gitlab.xiph.org/xiph/vorbis.git | grep "refs/tags/v[0-9]+\.[0-9]+\.[0-9]+$" -oE | sed 's/.*v//g' | sort -V | tail -n 1)
+	ver_check "$git_ver" "$inst_ver" && return
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" && return
+	local lfs_vers=$(lfs_ver $name)
+	ver_check "$lfs_vers" "$inst_ver" && return
+	fver "$name" "$inst_ver"
+}
+version=$(get_version)
+depends=(glibc libogg)
+filename="$name-$version.tar.xz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://downloads.xiph.org/releases/vorbis/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+cmi --prefix=/usr --disable-static
+sudo install -v -m644 doc/Vorbis* /usr/share/doc/$direname
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
