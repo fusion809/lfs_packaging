14909d0aea8abddd86d4270810d9222fab410af1
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 07:12:15 +1000

    +pixmap

---
+++ pixman/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+name=pixman
+repo=lib$name/$name
+version=$(gh_ver $repo)
+depends=(glibc)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://www.cairographics.org/releases/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+mni --prefix=/usr --buildtype=release
+cd ../..
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
