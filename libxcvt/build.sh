4481c0be4b431cea944bd5296d4db88615821c62
Brenton Horne <brentonhorne77@gmail.com>
2026-09-08 13:51:26 +1000

    +libxcvt

---
+++ libxcvt/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+name=libxcvt
+version=$(xfd_ver $name)
+depends=(glibc)
+filename="$name-$version.tar.xz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://www.x.org/pub/individual/lib/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+options=(--prefix=/usr --buildtype=release)
+mni "${options[@]}"
+cd ../..
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
