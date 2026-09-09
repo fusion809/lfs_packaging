8a261067256bccebfbec709a0e9c2d6c75b62b9c
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 11:24:43 +1000

    +smartmontools

---
+++ smartmontools/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+name=smartmontools
+repo=$name/$name
+version=$(gh_ver $repo)
+depends=(gcc glibc systemd)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://downloads.sourceforge.net/smartmontools/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+cmi --prefix=/usr --sysconfdir=/etc --docdir=/usr/share/doc/$direname
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
