05e3c2303e05744f0c2a337fcb8ad81de5cc9fe3
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 11:06:54 +1000

    +mdadm

---
+++ mdadm/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+name=mdadm
+repo=md-raid-utilities/$name
+version=$(gh_ver $repo)
+depends=(glibc systemd)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+# Kernel options required
+if ! [[ -f $filename ]]; then
+	wget -c https://git.kernel.org/pub/scm/utils/mdadm/mdadm.git/snapshot/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+make -j$(nproc)
+sudo make BINDIR=/usr/sbin install
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
