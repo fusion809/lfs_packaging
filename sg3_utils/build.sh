2829a2b3805fcfb5eba312de5ea585e872f1ac07
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 11:01:46 +1000

    +sg3_utils

---
+++ sg3_utils/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+name=sg3_utils
+repo=doug-gilbert/$name
+version=$(gh_ver $repo)
+depends=(glibc)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://sg.danny.cz/sg/p/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+sudo autoreconf -fiv
+sudo chown $USER -R .
+cmi --prefix=/usr --disable-static
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
