35d84ed5b4eae87428fdd324cef5bbd3171bc8d4
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 11:40:41 +1000

    +traceroute

---
+++ traceroute/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+name=traceroute
+get_version() {
+	local inst_ver=$(pkgver $name)
+	local up_ver=$(wget -T 5 -t 1 -cqO- https://sourceforge.net/projects/traceroute/files/traceroute/ | grep "traceroute-[0-9]+\.[0-9]+\.[0-9]+" -oE | cut -d '-' -f 2 | sort -V | tail -n 1)
+	ver_check "$up_ver" "$inst_ver" && return
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" && return
+	local lfs_vers=$(lfs_ver $name)
+	ver_check "$lfs_vers" "$inst_ver" && return
+	fver "$name" "$inst_ver"
+}
+version=$(get_version)
+depends=(glibc)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://github.com/$repo/releases/download/$direname/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+make -j$(nproc)
+sudo su -c "make prefix=/usr install                                 &&
+ln -sv -f traceroute /usr/bin/traceroute6                &&
+ln -sv -f traceroute.8 /usr/share/man/man8/traceroute6.8 &&
+rm -fv /usr/share/man/man1/traceroute.1"
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
