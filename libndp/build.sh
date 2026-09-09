e76ade49b385352ccf113a5149c9a528ef09964d
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 10:03:40 +1000

    +libndp

---
+++ libndp/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+name=libndp
+repo=jpirko/$name
+version=$(gh_ver $repo)
+depends=(glibc)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c http://libndp.org/files/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+cmi --prefix=/usr --disable-static --sysconfdir=/etc --localstatedir=/var
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
