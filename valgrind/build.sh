557b6e36023b20071113ca6ddc096ecba1f65b2c
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 12:05:37 +1000

    +valgrind

---
+++ valgrind/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+name=valgrind
+get_version() {
+	local inst_ver=$(pkgver $name)
+	local up_ver=$(wget -cqO- -T 5 -t 1 https://sourceware.org/pub/valgrind | grep "valgrind-[0-9]+\.[0-9]+\.[0-9]+" -oE | cut -d '-' -f 2 | sort -V | tail -n 1)
+	ver_check "$up_ver" "$inst_ver" && return
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" && return
+	local lfs_vers=$(lfs_ver $name)
+	ver_check "$lfs_vers" "$inst_ver" && return
+	fver "$name" "$inst_ver"
+}
+depends=(glibc hwloc libevent libfabric numactl openmpi systemd)
+filename="$name-$version.tar.bz2"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://sourceware.org/pub/valgrind/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+sed -i 's|/doc/valgrind||' docs/Makefile.in
+cmi --prefix=/usr --datadir=/usr/share/doc/$direname
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
