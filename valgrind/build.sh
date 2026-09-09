54313a7844cb6a8f708f5fc92b875eb89286cd1c
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 12:18:09 +1000

    +version line to valgrind

--- valgrind/build.sh
+++ valgrind/build.sh
@@ -11,6 +11,7 @@ get_version() {
 	ver_check "$lfs_vers" "$inst_ver" && return
 	fver "$name" "$inst_ver"
 }
+version=$(get_version)
 depends=(glibc hwloc libevent libfabric numactl openmpi systemd)
 filename="$name-$version.tar.bz2"
 direname="${filename/.tar.*/}"
