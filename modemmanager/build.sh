64a947476ebd2c54adcd67154d2115e422bbc775
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 11:33:27 +1000

    +Modemmanager

---
+++ modemmanager/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+name=modemmanger
+repo=mobile-broadband/ModemManager
+version=$(gh_ver $repo)
+depends=(glib2 glibc libffi libgudev libmbim libqmi pcre2 polkit systemd util-linux zlib)
+filename="ModemManager-$version.tar.gz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://github.com/$repo/releases/download/$direname/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+options=(--prefix=/usr            \
+      --buildtype=release      \
+      -D bash_completion=false \
+      -D qrtr=false)
+mni "${options[@]}"
+cd ../..
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
