124b9a831004e3168ab2f7658f72fd1f74920367
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 10:26:29 +1000

    +xorg fonts

---
+++ font-ibm-type1/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+name=font-ibm-type1
+depends=(glibc)
+repo=$name/$name
+get_version() {
+	local inst_ver=$(pkgver $name)
+	local up_ver=$(wget -cqO- -T 5 -t 1 https://www.x.org/pub/individual/font/ | grep "$name-[0-9]+\.[0-9]+\.[0-9]+" -oE | sed "s/$name-//g" | sort -V | tail -n 1)
+	ver_check "$up_ver" "$inst_ver" && return
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" && return
+	local lfs_vers=$(lfs_ver $name)
+	ver_check "$lfs_vers" "$inst_ver" && return
+	fver "$name" "$inst_ver"
+}
+filename="$name-$version.tar.xz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://www.x.org/pub/individual/font/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+cmi --prefix=/usr
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
