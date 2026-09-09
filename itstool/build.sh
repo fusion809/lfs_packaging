b97c40982530869dce6d11b16efbb942273eb4fd
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 11:53:38 +1000

    +itstool

---
+++ itstool/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+name=itstool
+repo=$name/$name
+version=$(gh_ver $repo)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://github.com/$repo/releases/download/$direname/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+gap_patches "$name"
+PYTHON=/usr/bin/python3 ./autogen.sh --prefix=/usr
+maki
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
