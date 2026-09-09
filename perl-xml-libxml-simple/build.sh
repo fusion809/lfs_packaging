dd97b63a4fad82d0261a310bb6a15d96662bcc6c
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 10:46:42 +1000

    +perl-xml-libxml-simple

---
+++ perl-xml-libxml-simple/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+name=perl-xml-libxml-simple
+get_version() {
+	local inst_ver=$(pkgver $name)
+	local up_ver=$(wget -cqO- -T 5 -t 1 https://www.cpan.org/authors/id/M/MA/MARKOV | grep "XML-LibXML-Simple-[0-9]+\.[0-9]+" -oE | cut -d '-' -f 2 | sort -V | tail -n 1)
+	ver_check "$up_ver" "$inst_ver" && return
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" && return
+	local lfs_vers=$(lfs_ver $name)
+	ver_check "$lfs_vers" "$inst_ver" && return
+	fver "$name" "$inst_ver"
+}
+version=$(get_version)
+filename="XML-LibXML-Simple-$version.tar.gz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://www.cpan.org/authors/id/M/MA/MARKOV/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+perl Makefile.PL &&
+maki
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
