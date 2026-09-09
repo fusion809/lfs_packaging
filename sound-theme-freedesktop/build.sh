312ad12f24343bdf8c8d4b0c923da113ba72d83a
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 11:41:58 +1000

    Fixing versioning of sound-theme-freedesktop

--- sound-theme-freedesktop/build.sh
+++ sound-theme-freedesktop/build.sh
@@ -6,7 +6,7 @@ get_version() {
 	local inst_ver=$(pkgver $name)
 	local up_ver=$(wget -cqO- -T 5 -t 1 https://people.freedesktop.org/\~mccann/dist/ | grep "sound-theme-freedesktop-[0-9]+\.[0-9]+" -oE | sed 's/sound-theme-freedesktop-//g' | sort -V | tail -n 1)
 	ver_check "$up_ver" "$inst_ver" && return
-	echop $(gh_ver $repo)
+	echo $(gh_ver $repo)
 }
 filename="$name-$version.tar.bz2"
 direname="${filename/.tar.*/}"
