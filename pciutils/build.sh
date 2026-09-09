8ee3812364c42ea1b29e86136373f02fcadbf705
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 10:59:49 +1000

    +pciutils

---
+++ pciutils/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+name=pciutils
+repo=$name/$name
+version=$(gh_ver $repo)
+depends=(glibc kmod openssl systemd xz zlib zstd)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+if ! [[ -f $filename ]]; then
+	wget -c https://mj.ucw.cz/download/linux/pci/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+sed -r '/INSTALL/{/PCI_IDS|update-pciids /d; s/update-pciids.8//}' \
+    -i Makefile
+make -j$(nproc) PREFIX=/usr                \
+     SHAREDIR=/usr/share/hwdata \
+     SHARED=yes
+sudo su -c "make PREFIX=/usr                \
+     SHAREDIR=/usr/share/hwdata \
+     SHARED=yes                 \
+     install install-lib        &&
+
+chmod -v 755 /usr/lib/libpci.so"
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
