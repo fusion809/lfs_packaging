04d573d04944baf16d72378e9dee63f2d59a445a
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 10:05:28 +1000

    +libnl

---
+++ libnl/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+name=libnl
+repo=thom311/$name
+version=$(gh_ver $repo)
+depends=(glibc)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+# Kernel config options required
+if ! [[ -f $filename ]]; then
+	wget -c https://github.com/$repo/releases/download/libnl${version/./_/}/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+cmi --prefix=/usr --sysconfdir=/etc --disable-static
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
