f4cc62dddd5ba32b32a6af68bb9e4e8dbc0b9214
Brenton Horne <brentonhorne77@gmail.com>
2026-09-09 11:49:56 +1000

    +iptables

---
+++ iptables/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+name=iptables
+repo=cernekee/$name
+version=$(gh_ver $repo)
+depends=(glibc)
+filename="$name-$version.tar.xz"
+direname="${filename/.tar.*/}"
+# Kernel options are required, too
+if ! [[ -f $filename ]]; then
+	wget -c https://www.netfilter.org/projects/iptables/files/$filename
+fi
+rm -rf "$direname"
+tar xf "$filename"
+cd "$direname"
+cmi --prefix=/usr --disable-nftables --enable-libipq
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
