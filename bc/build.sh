7f783dae060559f834d0938032e6c7ca8396f351
Brenton Horne <brentonhorne77@gmail.com>
2026-09-12 08:55:31 +1000

    Fixing bc build

--- bc/build.sh
+++ bc/build.sh
@@ -12,7 +12,7 @@ fi
 rm -rf $direname
 tar xf $filename
 cd $direname
-CC='gcc -std=c99' cmi "--prefix=/usr -G -O3 -r"
+CC='gcc -std=c99' cmi --prefix=/usr -G -O3 -r
 cd ..
 rm -rf $filename $direname
 echo "$version" sudo tee /var/lib/custom-packages/$name
