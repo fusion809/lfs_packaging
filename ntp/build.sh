6538a0c79e0db8879470638baafbd11d9a78e236
Brenton Horne <brentonhorne77@gmail.com>
2026-09-11 10:48:14 +1000

    Making ntp and rust-bindgen versioning more robust

--- ntp/build.sh
+++ ntp/build.sh
@@ -2,7 +2,20 @@
 set -e
 name=ntp
 repo=$name-project/$name
-version=$(gh_ver $repo)
+get_version() {
+	local inst_ver=$(pkgver $name)
+	local up_ver=$(wget -cqO- -T 5 -t 1 https://github.com/$repo/tags | grep "NTP_[0-9_P]+" -oE | sed 's/NTP_//g' | grep "P" | tr 'P' 'p' | tr '_' '.' | sed 's/\.$//g' | sort -V | tail -n 1)
+	ver_check "$up_ver" "$inst_ver" && return
+	local git_ver=$(timeout 5 git ls-remote --tags --refs https://github.com/$repo.git | grep "refs/tags/NTP_[0-9P_]+" -oE | sed 's/.*NTP_//g' | tr 'P' 'p' | tr '_' '.' | grep "p" | sed 's/\.$//g' | sort -V | tail -n 1)
+	ver_check "$git_ver" "$inst_ver" && return
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" && return
+	local lfs_vers=$(lfs_ver $name)
+	ver_check "$lfs_vers" "$inst_ver" && return
+	fver "$name" "$inst_ver"
+}
+# gh_ver sort of works but sometimes errors
+version=$(get_version)
 majmVer=$(echo $version | sed -E 's/.[0-9p]+$//g')
 majVer=$(echo $version | cut -d '.' -f 1)
 depends=(glibc libcap libevent ncurses openssl readline)
--- rust-bindgen/build.sh
+++ rust-bindgen/build.sh
@@ -2,7 +2,20 @@
 set -e
 name=rust-bindgen
 repo=rust-lang/$name
-version=$(gh_ver $repo)
+# gh_ver sometimes reports an out-of-date version due to the releases page having older versions listed than are tagged
+get_version() {
+	local inst_ver=$(pkgver $name)
+	local up_ver=$(wget -cqO- -T -5 -t 1 https://github.com/$repo/tags | grep "v[0-9]+\.[0-9]+\.[0-9]+" -oE | sed 's/^v//g' | sort -V | tail -n 1)
+	ver_check "$up_ver" "$inst_ver" && return
+	local git_ver=$(timeout 5 git ls-remote --tags --refs https://github.com/$repo.git | grep "v[0-9]+\.[0-9]+\.[0-9]+" -oE | sed 's/^v//g' | sort -V | tail -n 1)
+	ver_check "$up_ver" "$inst_ver" && return
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" && return
+	local lfs_vers=$(lfs_ver $name)
+	ver_check "$lfs_vers" "$inst_ver" && return
+	fver "$name" "$inst_ver"
+}
+version=$(get_version)
 depends=(gcc glibc llvm)
 filename="$name-$version.tar.gz"
 direname="${filename/.tar.*/}"
