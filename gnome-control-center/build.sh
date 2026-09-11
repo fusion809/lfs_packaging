15397a978c9ea1b970ed6a0f7ef23ae468b3b590
Brenton Horne <brentonhorne77@gmail.com>
2026-09-12 09:48:29 +1000

    Tweaking gnome-control-center versioning so it won't show the 51.0 update until gnome-desktop gets updated

--- gnome-control-center/build.sh
+++ gnome-control-center/build.sh
@@ -2,7 +2,18 @@
 set -e
 name=gnome-control-center
 repo=GNOME/$name
-version=$(gh_ver $repo)
+get_version() {
+	ver=$(gh_ver $repo)
+	majVer=$(echo $ver | sed -E 's/\.[0-9]+$//g')
+	gdVer=$(pkgver gnome-desktop)
+	gdMajVer=$(echo $gdVer | sed -E 's/\.[0-9]+$//g')
+	if [[ "$majVer" == "$gdMajVer" ]]; then
+		echo $ver
+	else
+		echo $(pkgver $name)
+	fi
+}
+version=$(get_version)
 depends=(brotli bzip2 colord curl cyrus-sasl e2fsprogs elfutils expat fontconfig freetype fribidi gcc gdk-pixbuf glib2 glibc glycin gmp gnutls graphene graphite2 gst-plugins-bad gst-plugins-base gstreamer gtk4 harfbuzz ibus icu jansson json-glib keyutils libX11 libXcursor libXdamage libXext libXfixes libXi libXinerama libXrandr libXrender libXxf86vm libadwaita libcap libepoxy libevdev libffi libfyaml libgcrypt libgpg-error libgudev libidn2 libnotify libpciaccess libpng libpsl libtasn1 libunistring libwacom libxcrypt libxkbcommon libxml2 libxmlb libxshmfence mesa mitkrb nettle networkmanager nghttp2 nspr nss openldap openssl orc p11-kit pango pcre2 polkit sqlite systemd udisks util-linux vulkan-loader wayland xz zlib zstd)
 blfs_depends=(accountsservice avahi cairo colord-gtk cracklib cups flac gcr4 gnome-bluetooth gnome-desktop gsound lame lcms2 libXau libXdmcp libcanberra libdrm libgtop libjpeg-turbo libnma libogg libpwquality librest libseccomp libsecret libsndfile libsoup libtiff libunwind libvorbis libwebp libxcb llvm lm-sensors modemmanager mpg123 opus pixman pulseaudio spirv-tools upower webkitgtk)
 lfs_depends=(dbus libelf)
