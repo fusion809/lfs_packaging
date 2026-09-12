0d5c351f1faf1fb714bb576e91e1161b0faa0542
Brenton Horne <brentonhorne77@gmail.com>
2026-09-13 09:04:39 +1000

    blas-lapack: simplifying

--- blas-lapack/build.sh
+++ blas-lapack/build.sh
@@ -2,14 +2,10 @@
 set -e
 # Combine lapack and blas
 depends=()
-lfs_depends=(bash coreutils gcc glibc gzip make python sed tar)
-blfs_depends=(cmake gcc wget)
+depends=(bash coreutils gcc glibc gzip make python sed tar cmake gcc wget)
 name=blas-lapack
 repo="Reference-LAPACK/lapack"
 version=$(gh_com $repo)
-
-DOCS="LICENSE"
-
 CFLAGS="-O2 -fPIC"
 
 if ! which gfortran &> /dev/null; then
@@ -46,28 +42,21 @@ cd ..
 
 # cmake doesn't appear to let us build both shared and static libs
 # at the same time, so build it twice.
-if [ "${STATIC:-no}" != "no" ]; then
-  mkdir -p static
-  cd static
-    cmake \
-      -DCMAKE_Fortran_FLAGS:STRING="$CFLAGS" \
-      -DCMAKE_INSTALL_PREFIX=/usr \
-      -DCMAKE_BUILD_TYPE=None \
-      -DCMAKE_RULE_MESSAGES=OFF \
-      -DCMAKE_VERBOSE_MAKEFILE=TRUE \
-      -DCBLAS=ON \
-      -DLAPACKE=OFF \
-      -DBUILD_DEPRECATED=OFF \
-      ..
-    make -j$(nproc)
-    sudo make install/strip
-  cd ..
-fi
+mkdir -p static
+cd static
+  cmake \
+    -DCMAKE_Fortran_FLAGS:STRING="$CFLAGS" \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DCMAKE_BUILD_TYPE=None \
+    -DCMAKE_RULE_MESSAGES=OFF \
+    -DCMAKE_VERBOSE_MAKEFILE=TRUE \
+    -DCBLAS=ON \
+    -DLAPACKE=OFF \
+    -DBUILD_DEPRECATED=OFF \
+  ..
+  make -j$(nproc)
+  sudo make install/strip
+cd ../..
 
-sudo rm -rf /usr/share/doc/blas-*
-sudo rm -rf /usr/share/doc/$name-*
-sudo mkdir -p /usr/share/doc/$name-$version
-sudo cp -a $DOCS /usr/share/doc/$name-$version
-cd ..
 sudo rm -rf ${filename} $direname
 echo $version | sudo tee /var/lib/custom-packages/$name
