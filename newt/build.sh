6bb93a4369a98be939b54015bd9875459bfcc0e4
Brenton Horne <brentonhorne77@gmail.com>
2026-09-15 17:45:17 +1000

    Fixing newt to build against tcl9

--- newt/build.sh
+++ newt/build.sh
@@ -28,6 +28,26 @@ sed -e '/install -m 644 $(LIBNEWT)/ s/^/#/' \
     -e '/$(LIBNEWT):/,/rv/ s/^/#/'          \
     -e 's/$(LIBNEWT)/$(LIBNEWTSH)/g'        \
     -i Makefile.in
+sed -i \
+    -e 's/interp->result = malloc(200);/result = malloc(200);/' \
+    -e '/interp->freeProc = TCL_DYNAMIC;/d' \
+    -e 's/interp->result = "no dialog mode was specified";/Tcl_SetResult(interp, "no dialog mode was specified", TCL_STATIC);/' \
+    -e 's/interp->result = "multiple modes were specified";/Tcl_SetResult(interp, "multiple modes were specified", TCL_STATIC);/' \
+    -e 's/interp->result = "missing text parameter";/Tcl_SetResult(interp, "missing text parameter", TCL_STATIC);/' \
+    -e 's/interp->result = "height missing";/Tcl_SetResult(interp, "height missing", TCL_STATIC);/' \
+    -e 's/interp->result = "height is not a number";/Tcl_SetResult(interp, "height is not a number", TCL_STATIC);/' \
+    -e 's/interp->result = "width missing";/Tcl_SetResult(interp, "width missing", TCL_STATIC);/' \
+    -e 's/interp->result = "width is not a number";/Tcl_SetResult(interp, "width is not a number", TCL_STATIC);/' \
+    -e 's/interp->result = "list-height missing";/Tcl_SetResult(interp, "list-height missing", TCL_STATIC);/' \
+    -e 's/interp->result = "list-height is not a number";/Tcl_SetResult(interp, "list-height is not a number", TCL_STATIC);/' \
+    -e 's/interp->result = "yes";/Tcl_SetResult(interp, "yes", TCL_STATIC);/' \
+    -e 's/interp->result = "no";/Tcl_SetResult(interp, "no", TCL_STATIC);/' \
+    -e 's/interp->result = result;/Tcl_SetResult(interp, result, TCL_DYNAMIC);/' \
+    -e 's/interp->result = selections\[0\];/Tcl_SetResult(interp, selections[0], TCL_DYNAMIC);/' \
+    -e 's/interp->result = "bad paramter for whiptcl dialog box";/Tcl_SetResult(interp, "bad paramter for whiptcl dialog box", TCL_STATIC);/' \
+    -e 's/Tcl_FreeResult(interp);/Tcl_ResetResult(interp);/' \
+    -e 's/sprintf(interp->result, "%s: %s\\n",/sprintf(result, "%s: %s\\n",/' \
+    whiptcl.c
 cmi --prefix=/usr --with-gpm-support
 cd ../
 rm -rf "$filename" "$direname"
