f0e1fb8dd45bc147392ce8a2895aa009119c3d96
Brenton Horne <brentonhorne77@gmail.com>
2026-09-17 08:56:43 +1000

    Fixing pam perm issues that sometimes happen with package reinstalls

--- sddm/build.sh
+++ sddm/build.sh
@@ -22,6 +22,48 @@ options=(-D CMAKE_INSTALL_PREFIX=/usr        \
       -D DATA_INSTALL_DIR=/usr/share/sddm \
       -D DBUS_CONFIG_FILENAME=sddm_org.freedesktop.DisplayManager.conf)
 cmaki "${options[@]}"
+sudo tee /etc/pam.d/sddm > /dev/null << 'PAMEOF'
+# Begin /etc/pam.d/sddm
+auth        requisite   pam_nologin.so
+auth        required    pam_env.so
+auth        required    pam_succeed_if.so uid >= 1000 quiet
+auth        include     system-auth
+-auth       optional    pam_gnome_keyring.so
+-auth       optional    pam_kwallet5.so
+
+account     include     system-account
+
+password    include     system-password
+-password   optional    pam_gnome_keyring.so use_authtok
+
+session     required    pam_limits.so
+#session     include     system-session
+session include system-login
+-session    optional    pam_gnome_keyring.so auto_start
+-session    optional    pam_kwallet5.so auto_start
+# End /etc/pam.d/sddm
+PAMEOF
+sudo tee /etc/pam.d/sddm-autologin > /dev/null << 'PAMEOF'
+# Begin /etc/pam.d/sddm-autologin
+auth        requisite   pam_nologin.so
+auth        required    pam_env.so
+auth        required    pam_succeed_if.so uid >= 1000 quiet
+auth        required    pam_permit.so
+-auth       optional    pam_gnome_keyring.so
+-auth       optional    pam_kwallet5.so
+
+account     include     system-account
+
+password    required    pam_deny.so
+
+session     required    pam_limits.so
+#session     include     system-session
+session include system-login
+-session    optional    pam_gnome_keyring.so auto_start
+-session    optional    pam_kwallet5.so auto_start
+# End /etc/pam.d/sddm-autologin
+PAMEOF
+
 cd ../..
 rm -rf "$filename" "$direname"
 echo "$version" | sudo tee "/var/lib/custom-packages/$name"
--- shadow/build.sh
+++ shadow/build.sh
@@ -28,7 +28,7 @@ sudo make exec_prefix=/usr pamddir= install
 sudo make -C man install-man
 cd ..
 sudo su -c '
-for FILE in chage chpasswd login newusers passwd su
+for FILE in chage chpasswd newusers passwd su
 do
 	install -vm644 $FILE /etc/pam.d/$FILE
 done
@@ -39,5 +39,38 @@ do
     install -v -m644 /etc/pam.d/chage /etc/pam.d/${PROGRAM}
     sed -i "s/chage/$PROGRAM/" /etc/pam.d/${PROGRAM}
 done'
+sudo tee /etc/pam.d/login > /dev/null <<'EOF'
+#%PAM-1.0
+
+# Set failure delay before next prompt to 3 seconds
+auth      optional    pam_faildelay.so  delay=3000000
+
+# Check to make sure that the user is allowed to login
+auth      requisite   pam_nologin.so
+
+# Check to make sure that root is allowed to login
+# Disabled by default. You will need to create /etc/securetty
+# file for this module to function.
+#auth      required    pam_securetty.so
+
+# include system auth settings
+auth      include     system-auth
+
+# check access for the user
+account   required    pam_access.so
+
+# include system account settings
+account   include     system-account
+
+# Set default environment variables for the user
+session   required    pam_env.so
+
+# Set resource limits for the user
+session   required    pam_limits.so
+
+# include system session and password settings
+session   include     system-session
+password  include     system-password
+EOF
 rm -rf $filename $direname
 echo $version | sudo tee /var/lib/custom-packages/$name
