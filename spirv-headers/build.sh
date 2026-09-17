d39ccf7be7a3f09a9bc24b2b5691d7434e560fb2
Brenton Horne <brentonhorne77@gmail.com>
2026-09-17 21:26:21 +1000

    Fixing spirv-headers versioning

--- spirv-headers/build.sh
+++ spirv-headers/build.sh
@@ -3,7 +3,20 @@ set -e
 name=spirv-headers
 _name=SPIRV-Headers-vulkan-sdk
 repo=KhronosGroup/SPIRV-Headers
-version=$(gh_ver $repo $name)
+get_version() {
+	local inst_ver=$(pkgver $name)
+	local git_ver=$(timeout 5 git ls-remote --tags --refs https://github.com/$repo.git | grep -oE "[0-9]\.[0-9]\.[0-9]+\.[0-9]" | sort -V | tail -n 1)
+	ver_check "$git_ver" "$inst_ver" && return
+	local vat_ver=$(vatver $name)
+	ver_check "$vat_ver" "$inst_ver" && return
+
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" && return
+	local lfs_vers=$(lfs_ver $name)
+	ver_check "$lfs_vers" "$inst_ver" && return
+	fver "$name" "$inst_ver"
+}
+version=$(get_version)
 filename="$_name-$version.tar.gz"
 direname="${filename/.tar.*/}"
 depends=(cmake)
