c2ce7df9ea5b06a853ec99fa33bfc42ac0ddfbf4
Brenton Horne <brentonhorne77@gmail.com>
2026-09-19 11:02:25 +1000

    Modularizing openjpeg

--- openjpeg/build.sh
+++ openjpeg/build.sh
@@ -6,17 +6,13 @@ version=$(gh_ver $repo)
 depends=(cmake glibc lcms2 libjpeg-turbo libpng libtiff libwebp xz zlib zstd)
 filename="$name-$version.tar.gz"
 direname="${filename/.tar.*/}"
-if ! [[ -f $filename ]]; then
-	wget -c --progress=bar:force https://github.com/$repo/archive/v$version/$filename
-fi
-rm -rf "$direname"
-tar xf "$filename"
-cd "$direname"
+gha_download "$repo" "v$version" "$filename"
+unpk_enter "$filename" "$direname"
 options=(-D CMAKE_BUILD_TYPE=Release  \
       -D CMAKE_INSTALL_PREFIX=/usr \
       -D BUILD_STATIC_LIBS=OFF)
 cmaki "${options[@]}"
 sudo cp -rv ../doc/man -T /usr/share/man
-cd ../
+cd ../..
 rm -rf "$filename" "$direname"
 echo "$version" | sudo tee "/var/lib/custom-packages/$name"
