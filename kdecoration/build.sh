8946cad9098e593ef3d40eb8a8023d5171a92a53
Brenton Horne <brentonhorne77@gmail.com>
2026-09-19 14:01:20 +1000

    Modularizing build scripts

--- kdecoration/build.sh
+++ kdecoration/build.sh
@@ -6,12 +6,8 @@ version=$(gh_ver $repo)
 depends=(brotli bzip2 dbus double-conversion expat fontconfig freetype gcc glib2 glibc graphite2 harfbuzz icu ki18n libdrm libelf libffi libpciaccess libpng libX11 libXau libxcb libXdmcp libXext libxkbcommon libxml2 libxshmfence libXxf86vm llvm lm-sensors mesa pcre2 qt6 spirv-tools systemd wayland xz zlib zstd)
 filename="$name-$version.tar.gz"
 direname="${filename/.tar.*/}"
-if ! [[ -f $filename ]]; then
-	wget -c --progress=bar:force https://github.com/$repo/archive/refs/tags/v$version.tar.gz -O $filename
-fi
-rm -rf "$direname"
-tar xf "$filename"
-cd "$direname"
+gha_download "$repo" "v$version" "$filename"
+unpk_enter "$filename" "$direname"
 cmaki -D CMAKE_INSTALL_PREFIX=/usr -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_LIBEXECDIR=libexec -D BUILD_QT5=OFF -D BUILD_TESTING=OFF
 cd ../..
 rm -rf "$filename" "$direname"
