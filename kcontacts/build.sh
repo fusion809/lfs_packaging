2268d0ac85c4c895c5c100722e36015249ed2be4
Brenton Horne <brentonhorne77@gmail.com>
2026-09-19 13:53:38 +1000

    Modularizing build scripts

--- kcontacts/build.sh
+++ kcontacts/build.sh
@@ -4,23 +4,19 @@ name=kcontacts
 repo=KDE/$name
 version=$(gh_ver $repo)
 depends=(brotli bzip2 dbus double-conversion e2fsprogs expat fontconfig freetype gcc glib2 glibc graphite2 harfbuzz icu kcodecs kconfig keyutils ki18n libdrm libelf libffi libpciaccess libpng libX11 libXau libxcb libXdmcp libXext libxkbcommon libxml2 libxshmfence libXxf86vm llvm lm-sensors mesa mitkrb openssl pcre2 qt6 spirv-tools systemd wayland xz zlib zstd)
-majVer=$(echo $version | sed -E 's/\.[0-9]+$//g')
 filename="$name-$version.tar.xz"
 direname="${filename/.tar.*/}"
-if ! [[ -f $filename ]]; then
-	wget -c --progress=bar:force https://download.kde.org/stable/frameworks/$majVer/$filename
-fi
-rm -rf "$direname"
-tar xf "$filename"
-cd "$direname"
-cmake_options=(-D CMAKE_INSTALL_PREFIX=/usr \
-            -D CMAKE_INSTALL_LIBEXECDIR=libexec \
-            -D CMAKE_PREFIX_PATH=/opt/qt6        \
-            -D CMAKE_SKIP_INSTALL_RPATH=ON      \
-            -D CMAKE_BUILD_TYPE=Release         \
-            -D BUILD_TESTING=OFF                \
-            -D BUILD_PYTHON_BINDINGS=OFF        \
-	    -W no-author)
+kde_download "frameworks" "$version" "$filename"
+unpk_enter "$filename" "$direname"
+cmake_options=(
+    -D CMAKE_INSTALL_PREFIX=/usr \
+    -D CMAKE_INSTALL_LIBEXECDIR=libexec \
+    -D CMAKE_PREFIX_PATH=/opt/qt6        \
+    -D CMAKE_SKIP_INSTALL_RPATH=ON      \
+    -D CMAKE_BUILD_TYPE=Release         \
+    -D BUILD_TESTING=OFF                \
+    -D BUILD_PYTHON_BINDINGS=OFF        \
+	-W no-author)
 cmaki "${cmake_options[@]}"
 cd ../..
 rm -rf "$filename" "$direname"
--- kde-gtk-config/build.sh
+++ kde-gtk-config/build.sh
@@ -6,12 +6,8 @@ version=$(gh_ver $repo)
 depends=(at-spi2-core brotli bzip2 cairo dbus double-conversion expat fontconfig freetype fribidi gcc gdk-pixbuf glib2 glibc glycin graphite2 gtk3 harfbuzz icu kcolorscheme kconfig kcoreaddons kdbusaddons kdecoration kguiaddons ki18n kwindowsystem lcms2 libdrm libelf libepoxy libffi libpciaccess libpng libseccomp libX11 libXau libxcb libXcomposite libXcursor libXdamage libXdmcp libXext libXfixes libXi libXinerama libxkbcommon libxml2 libXrandr libXrender libXres libxshmfence libXxf86vm llvm lm-sensors mesa pango pcre2 pixman qt6 spirv-tools systemd util-linux wayland xcb-util-keysyms xz zlib zstd)
 filename="$name-$version.tar.gz"
 direname="${filename/.tar.*/}"
-if ! [[ -f $filename ]]; then
-	wget -c --progress=bar:force https://github.com/$repo/archive/refs/tags/v$version.tar.gz -O $filename
-fi
-rm -rf "$direname"
-tar xf "$filename"
-cd "$direname"
+gha_download "$repo" "v$version" "$filename"
+unpk_enter "$filename" "$direname"
 cmaki -D CMAKE_INSTALL_PREFIX=/usr -D CMAKE_BUILD_TYPE=Release -D CMAKE_INSTALL_LIBEXECDIR=libexec -D BUILD_QT5=OFF -D BUILD_TESTING=OFF
 cd ../..
 rm -rf "$filename" "$direname"
