b47b30799cb70625304a70b041f305d55e6f5c07
Brenton Horne <brentonhorne77@gmail.com>
2026-09-21 14:20:55 +1000

    Tweaks to zlib versioning

--- zlib/build.sh
+++ zlib/build.sh
@@ -6,6 +6,10 @@ get_up_ver() {
 	local lfs_vers=$(lfs_ver $name)
 	local up_ver=$(wget -T 5 -t 1 -cqO- https://zlib.net/fossils | grep -E "zlib-[0-9]+\.[0-9]+" | cut -d '"' -f 4 | cut -d '-' -f 2 | sed 's/.tar.gz//g' | sort -V | tail -n 1)
 	ver_check "$up_ver" "$inst_ver" "$lfs_vers" && return
+	local arch_ver=$(aver $name)
+	ver_check "$arch_ver" "$inst_ver" "$lfs_vers" && return
+	local art_ver=$(aver $name)
+	ver_check "$art_ver" "$inst_ver" "$lfs_vers" && return
 }
 repo=malder/$name
 version=$(get_up_ver || gh_ver $repo)
