d453e091fad3230f394e3ad739dfcbb02bcde39c
Brenton Horne <brentonhorne77@gmail.com>
2026-09-22 10:54:44 +1000

    Attempted fix for vte versioning faulure

--- vte/build.sh
+++ vte/build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 name=vte
-version=$(gn_ver vte)
+get_version() {
+	local ver=$(gn_ver $name)
+	if [[ $ver == "2.91" ]]; then
+		ver=$(pkgver $name)
+	fi
+}
+version=$(get_version)
 filename="$name-$version.tar.gz"
 direname="${filename/.tar.*/}"
 depends=(fast_float fmt glib2 gnutls gtk3 gtk4 icu libxml2 simdutf vala)
