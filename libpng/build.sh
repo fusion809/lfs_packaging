59ba0d4438ca3a426fcfaf258786aef2d63f8f4b
Brenton Horne <brentonhorne77@gmail.com>
2026-09-26 09:58:10 +1000

    Fixing libpng doc direname

--- libpng/build.sh
+++ libpng/build.sh
@@ -12,7 +12,7 @@ download_src "https://downloads.sourceforge.net/sourceforge/libpng-apng/$patch_f
 unpk_enter "$filename" "$direname"
 zcat ../$patch_filename | patch -p1
 cmi --prefix=/usr --disable-static
-sudo install -vDm644 README libpng-manual.txt -t /usr/share/doc/$filename
+sudo install -vDm644 README libpng-manual.txt -t /usr/share/doc/$direname
 cd ..
 rm -rf "$filename" "$direname" "$patch_filename"
 echo "$version" | sudo tee "/var/lib/custom-packages/$name"
