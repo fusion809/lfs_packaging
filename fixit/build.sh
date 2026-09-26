7b985ba7e368d1e5f2048e52ed96b05cd32f6c46
Brenton Horne <brentonhorne77@gmail.com>
2026-09-26 17:52:50 +1000

    +fixit

---
+++ fixit/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+name=fixit
+repo=eugene-babichenko/$name
+version=$(gh_ver $repo)
+filename="$name-$version.tar.gz"
+direname="${filename/.tar.*/}"
+depends=(gcc glibc rust)
+gha_download "$repo" "v$version" "$filename"
+unpk_enter "$filename" "$direname"
+export RUSTUP_TOOLCHAIN=stable
+export CARGO_TARGET_DIR=target
+cargo build --locked --release
+sudo install -Dm755 target/release/$name -t /usr/bin
+cd ../
+rm -rf "$filename" "$direname"
+echo "$version" | sudo tee "/var/lib/custom-packages/$name"
